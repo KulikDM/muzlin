146538bb6cb4556d664751c15c419d6292480576
KulikDM <rogerplanck8@gmail.com>
2024-11-17 19:50:54 +0200

    Add client initialization to cohere encoder

--- muzlin/encoders/cohere.py
+++ muzlin/encoders/cohere.py
@@ -1,7 +1,7 @@
 import os
 from typing import List, Optional
 
-import cohere
+from pydantic.v1 import PrivateAttr
 
 from muzlin.encoders import BaseEncoder
 from muzlin.utils.defaults import EncoderDefault
@@ -10,40 +10,69 @@ from muzlin.utils.defaults import EncoderDefault
 
 
 class CohereEncoder(BaseEncoder):
-    client: Optional[cohere.Client] = None
-    type: str = 'cohere'
-    input_type: Optional[str] = 'search_query'
+    _client: Any = PrivateAttr()
+    _embed_type: Any = PrivateAttr()
+    type: str = "cohere"
+    input_type: Optional[str] = "search_query"
 
     def __init__(
         self,
         name: Optional[str] = None,
         cohere_api_key: Optional[str] = None,
-        input_type: Optional[str] = 'search_query',
+        input_type: Optional[str] = "search_query",
     ):
         if name is None:
-            name = EncoderDefault.COHERE.value['embedding_model']
+            name = EncoderDefault.COHERE.value["embedding_model"]
         super().__init__(
             name=name,
             input_type=input_type,  # type: ignore
         )
         self.input_type = input_type
-        cohere_api_key = cohere_api_key or os.getenv('COHERE_API_KEY')
+        self._client = self._initialize_client(cohere_api_key)
+
+    def _initialize_client(self, cohere_api_key: Optional[str] = None):
+        """Initializes the Cohere client.
+
+        :param cohere_api_key: The API key for the Cohere client, can also
+        be set via the COHERE_API_KEY environment variable.
+
+        :return: An instance of the Cohere client.
+        """
+        try:
+            import cohere
+            from cohere.types.embed_response import EmbeddingsByTypeEmbedResponse
+
+            self._embed_type = EmbeddingsByTypeEmbedResponse
+        except ImportError:
+            raise ImportError(
+                "Please install Cohere to use CohereEncoder. "
+                "You can install it with: "
+                "`pip install cohere`"
+            )
+        cohere_api_key = cohere_api_key or os.getenv("COHERE_API_KEY")
         if cohere_api_key is None:
             raise ValueError("Cohere API key cannot be 'None'.")
         try:
-            self.client = cohere.Client(cohere_api_key)
+            client = cohere.Client(cohere_api_key)
         except Exception as e:
             raise ValueError(
                 f"Cohere API client failed to initialize. Error: {e}"
             ) from e
+        return client
 
     def __call__(self, docs: List[str]) -> List[List[float]]:
-        if self.client is None:
-            raise ValueError('Cohere client is not initialized.')
+        if self._client is None:
+            raise ValueError("Cohere client is not initialized.")
         try:
-            embeds = self.client.embed(
-                docs, input_type=self.input_type, model=self.name
+            embeds = self._client.embed(
+                texts=docs, input_type=self.input_type, model=self.name
             )
-            return embeds.embeddings
+            # Check for unsupported type.
+            if isinstance(embeds, self._embed_type):
+                raise NotImplementedError(
+                    "Handling of EmbedByTypeResponseEmbeddings is not implemented."
+                )
+            else:
+                return embeds.embeddings
         except Exception as e:
             raise ValueError(f"Cohere API call failed. Error: {e}") from e
